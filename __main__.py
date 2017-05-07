7a0e82343f5815baabceb826b2930ae0e48a19e2
Cesar Prados <bradomyn@gmail.com>
2017-05-07 22:39:08 +0200

    main:remove test file replaces with main

--- __main__.py
+++ __main__.py
@@ -1,9 +1,12 @@
-"""Module docstring.
-
-This serves as a long usage message.
+"""
+Scheduler toy program.
 """
 import sys
 import getopt
+from fg import frame_gen
+from output_queue import output_queue 
+from scheduler import random_queue
+
 
 def main():
     # parse command line options
@@ -21,6 +24,32 @@ def main():
     # process arguments
     for arg in args:
         process(arg) # process() is defined elsewhere
+
+    fg = frame_gen()
+    q = output_queue(8,10)
+    scheduler = random_queue()
+
+    for x in range(10000):
+
+        pay_load = fg.frame_pay_load()
+        qos = fg.frame_qos()
+        
+        q.add_frame_queue(qos, pay_load)
+        
+        tx_queue = scheduler.next_q()
+
+        q.tx_frame_queue(tx_queue)
+
+
+        
+    print list(q.out_queue[0])
+    print list(q.out_queue[1])
+    print list(q.out_queue[2])
+    print list(q.out_queue[3])
+    print list(q.out_queue[4])
+    print list(q.out_queue[5])
+    print list(q.out_queue[6])
+    print list(q.out_queue[7])
 
 if __name__ == "__main__":
     main()
